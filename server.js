40127f07b1824b6ee16e2b56f459bdb22f721884
Fuad Chonora <fuadchonora@gmail.com>
2019-07-24 15:18:56 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,144 @@
+let express = require('express');
+var mysql = require('mysql');
+var bodyParser = require("body-parser");
+
+let router = express.Router();
+let app = express();
+
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.post('/add-class',function(req,res){
+  var class_name=req.body.class_name;
+  var tutor_id=req.body.tutor_id;
+  console.log("Class Name is "+class_name+", tutor id is "+tutor_id);
+
+  var sql = "INSERT INTO class (var_class_name, fk_int_tutor_id) VALUES ?";
+  var values = [
+    [class_name, tutor_id]
+  ];
+  connection.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+    res.end("Inserted");
+  });
+  
+});
+
+app.post('/add-subject',function(req,res){
+  var sub_name=req.body.sub_name;
+  var teacher_id=req.body.teacher_id;
+  console.log("Subject Name is "+sub_name+", teacher id is "+teacher_id);
+
+  var sql = "INSERT INTO subject (var_sub_name, fk_int_teacher_id) VALUES ?";
+  var values = [
+    [sub_name, teacher_id]
+  ];
+
+  connection.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+    res.end("Inserted");
+  });
+  
+});
+
+app.post('/add-student',function(req,res){
+  let class_id=req.body.class_id;
+  let reg_no=req.body.reg_no;
+  let std_name=req.body.std_name;
+  let std_dob=req.body.std_dob;
+  let std_gender=req.body.std_gender;
+  let std_ph=req.body.std_ph;
+  let std_email=req.body.std_email;
+  let std_address=req.body.std_address;
+  
+  console.log("Student Name is "+std_name);
+
+  var sql = "INSERT INTO students (fk_int_class_id, int_reg_no, var_std_name,date_std_dob, var_std_gender, var_std_ph, var_std_email, var_std_address) VALUES ?";
+  var values = [
+    [class_id, reg_no, std_name, std_dob, std_gender, std_ph, std_email,std_address]
+  ];
+
+  connection.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+    res.end("Inserted");
+  });
+  
+});
+
+app.post('/add-teacher',function(req,res){
+  let teacher_name=req.body.teacher_name;
+  let teacher_gender=req.body.teacher_gender;
+  let teacher_dob=req.body.teacher_dob;
+  let teacher_ph=req.body.teacher_ph;
+  let teacher_email=req.body.teacher_email;
+  let teacher_address=req.body.teacher_address;
+  let teacher_username=req.body.teacher_username;
+  let teacher_password=req.body.teacher_password;
+  
+  let teacher_status=0;
+
+  
+  console.log("Teacher Name is "+teacher_name);
+
+  var sql = "INSERT INTO teacher (var_teacher_name, var_teacher_gender, date_teacher_dob, var_teacher_ph, var_teacher_email, var_teacher_address, var_teacher_username, var_teacher_password, var_teacher_status) VALUES ?";
+  var values = [
+    [teacher_name, teacher_gender, teacher_dob, teacher_ph, teacher_email, teacher_address, teacher_username, teacher_password, teacher_status]
+  ];
+
+  connection.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+    res.end("Inserted");
+  });
+  
+});
+
+app.post('/add-time-table',function(req,res){
+  let tt_id=req.body.tt_id;
+  let teacher_id=req.body.teacher_id;
+  let tt_day=req.body.tt_day;
+  let tt_period=req.body.tt_period;
+  let class_id=req.body.class_id;
+
+  
+  console.log("Teacher Id is "+teacher_id);
+
+  var sql = "INSERT INTO time_table ( pk_int_tt_id, fk_int_teacher_id, var_day, var_period, fk_int_class_id) VALUES ?";
+  var values = [
+    [tt_id,teacher_id,tt_day,tt_period,class_id]
+  ];
+
+  connection.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+    res.end("Inserted");
+  });
+  
+});
+
+
+connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'tution_center'
+});
+
+connection.connect(function(err) {
+    if(err){
+      console.log('Database Connection Error.!');
+      throw err;
+    }
+    console.log('Database Connected.');
+        
+    app.listen(3000,function(){
+      console.log("Started on PORT 3000");
+    });
+    
+});
